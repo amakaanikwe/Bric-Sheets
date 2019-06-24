96bace1497a743214725ec2c836e1f105bf06357
Amaka Anikwe <amakaanikwe@gmail.com>
2019-06-23 20:47:45 -0400

    Css style updates

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -15,7 +15,7 @@ app.controller("SearchController", [
       $scope.openModal = function(person) {
         let modalInstance = $uibModal.open({
           animation: true,
-          size: "sm",
+          size: "lg",
           ariaLabelledBy: "modal-title",
           ariaDescribedBy: "modal-body",
           templateUrl: "public/js/partials/appModal.html",
