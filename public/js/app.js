d24acfb064eaf2cf758c5a33347522392a20426e
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-24 08:15:35 -0500

    Updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const app = angular.module('bricApp',['ngRoute', 'ui.bootstrap']);
 app.config (['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'public/js/partials/searchList.html',
+      templateUrl: './js/partials/searchList.html',
       controller: 'SearchController'
     })
 
