089c7a838216e96b59ad213c145e8fbf46ec4950
Amaka Anikwe <amakaanikwe@gmail.com>
2020-03-24 07:21:12 -0400

    Updated server file

---
+++ public/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const path = require("path");
+const log = console.log;
+
+const PORT = 8080;
+
+// app.use(express.static(“myApp”)); 
+app.use(express.static(__dirname + '/public'));
+
+//Static Folder
+app.use('/public', express.static(path.join(__dirname, "public")));
+
+
+
+app.listen(PORT, () => log("Server is running on PORT,", 8080));
