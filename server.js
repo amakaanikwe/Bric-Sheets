ec1897919003ab9f2cb676e763029278a21bb0d4
Amaka Anikwe <amakaanikwe@gmail.com>
2020-03-27 08:39:25 -0400

    Updated server

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const path = require("path");
 const log = console.log;
 
-const PORT = 8080;
+const PORT = 8081;
 
 // app.use(express.static(“myApp”)); 
+app.use(express.static(__dirname + '/public'));
 
 //Static Folder
-app.use('/public', express.static(path.join(__dirname, "public")));
+app.use('./public', express.static(path.join(__dirname, "public")));
 
 
 
