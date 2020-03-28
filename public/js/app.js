5497f3e18c8fb9626d00be249468385cea1f3a7f
Amaka Anikwe <amakaanikwe@gmail.com>
2020-03-28 13:20:31 -0400

    Updated js files

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,20 @@ const app = angular.module('bricApp',['ngRoute', 'ui.bootstrap']);
 
 app.config (['$routeProvider', function($routeProvider) {
   $routeProvider
-    .when('/', {
+    .when('/login', {
+      templateUrl: '../modules/login_registration/views/login.html',
+      controller: 'RegistrationController'
+    })
+    .when('/registration', {
+      templateUrl: '../modules/login_registration/views/registration.html',
+      controller: 'RegistrationController'
+    })
+    .when('/success', {
       templateUrl: './public/js/partials/searchList.html',
       controller: 'SearchController'
+    })
+    .otherwise({
+      redirectTo: '/login'
     })
 
 }]);
