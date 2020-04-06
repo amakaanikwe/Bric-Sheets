cbde90e1f7add53af5e9eb3181b6ed06e102a6fa
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-06 09:51:49 -0400

    Updated reg controller

--- src/modules/login_registration/registrationController.js
+++ src/modules/login_registration/registrationController.js
@@ -1,12 +1,22 @@
-app.controller('RegistrationController', ['$scope', function($scope){
+app.controller('RegistrationController', ['$scope','$firebase', '$firebaseAuth', function($scope){
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
     
     $scope.login = function() {
         $scope.message = "Welcome " + $scope.user.email;
     };
 
     $scope.register = function() {
-        $scope.message = "Welcome" + $scope.user.firstName;
-    }
+        auth.$createUserWithEmailAndPassword(
+            $scope.user.email,
+            $scope.user.password
+        ).then(function(regUser){
+            $scope.message = "Welcome" + $scope.user.firstName;
+        }).catch(function(error) {
+            $scope.message = error.message;
+        }); // createUserWithEmailAndPassword  
+         
+    };//register
     
-
-}]);
+}]);//Controller
