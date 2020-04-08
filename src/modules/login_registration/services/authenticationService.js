58d4c9b56e94b85d2fb854040203a46495a85366
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-07 23:54:09 -0400

    Updated service

--- src/modules/login_registration/services/authenticationService.js
+++ src/modules/login_registration/services/authenticationService.js
@@ -2,4 +2,16 @@ app.factory('Authentication',
 ['$rootScope', '$firebaseAuth',
 function($rootScope, $firebaseAuth) {
 
-}])
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+
+    return {
+        login: function(user) {
+
+        }, //login
+        register: function(user) {
+
+        } //register
+    }
+
+}]); //factory
