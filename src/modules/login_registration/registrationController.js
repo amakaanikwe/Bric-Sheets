8538cb3826688ef442b78d8ed884f6dc88125412
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-07 23:53:39 -0400

    Updated AuthFactory.js

--- src/modules/login_registration/registrationController.js
+++ src/modules/login_registration/registrationController.js
@@ -1,7 +1,5 @@
 app.controller('RegistrationController', ['$scope','$firebase', '$firebaseAuth', function($scope){
 
-    var ref = firebase.database().ref();
-    var auth = $firebaseAuth();
     
     $scope.login = function() {
         $scope.message = "Welcome " + $scope.user.email;
@@ -16,7 +14,7 @@ app.controller('RegistrationController', ['$scope','$firebase', '$firebaseAuth',
         }).catch(function(error) {
             $scope.message = error.message;
         }); // createUserWithEmailAndPassword  
-         
+
     };//register
     
 }]);//Controller
