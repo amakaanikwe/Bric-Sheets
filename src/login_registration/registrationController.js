b31934b0465c17d2cf9d17bbaac1b9830fdd8002
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-19 13:52:22 -0400

    Updated src/login_registration

---
+++ src/login_registration/registrationController.js
@@ -0,0 +1,13 @@
+app.controller("RegistrationController", [
+  "$scope",
+  "Authentication",
+  function ($scope, Authentication) {
+    $scope.login = function () {
+      Authentication, login($scope.user);
+    };
+
+    $scope.register = function () {
+      Authentication.register($scope.user);
+    }; //register
+  },
+]); //Controller
---
+++ src/login_registration/services/authenticationService.js
@@ -0,0 +1,31 @@
+app.factory("Authentication", [
+  "$rootScope",
+  "$firebaseAuth",
+  function ($rootScope, $firebaseAuth) {
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+
+    return {
+      login: function (user) {
+        $rootScope.message = "Welcome " + $rootScope.user.email;
+      }, //login
+      register: function (user) {
+        auth
+          .$createUserWithEmailAndPassword(user.email, user.password)
+          .then(function (regUser) {
+            var regRef = ref.child("users").child(regUser.uid).set({
+              date: firebase.database.ServerValue.TIMESTAMP,
+              regUser: regUser.uid,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              email: user.email,
+            }); //userinfo
+            $rootScope.message = "Welcome" + user.firstName;
+          })
+          .catch(function (error) {
+            $rootScope.message = error.message;
+          }); // createUserWithEmailAndPassword
+      }, //register
+    };
+  },
+]); //factory
