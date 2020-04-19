5e1341bc6eadc4b468101437eafe6e83a07dcfd9
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-19 13:52:40 -0400

    Updated app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = angular.module('bricApp',['ngRoute','firebase','ui.bootstrap']);
+
+app.config (['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/login', {
+      templateUrl: '../src/login_registration/views/login.html',
+      controller: 'RegistrationController'
+    })
+    .when('/registration', {
+      templateUrl: '../src/login_registration/views/registration.html',
+      controller: 'RegistrationController'
+    })
+    .when('/success', {
+      templateUrl: './public/js/partials/searchList.html',
+      controller: 'SearchController'
+    })
+    .otherwise({
+      redirectTo: '/login'
+    })
+
+}]);
