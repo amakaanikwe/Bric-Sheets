1d07f3af5e2923a96da40f706179ab0bbbf122f5
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-20 05:48:39 -0400

    Added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const browserSync = require("browser-sync").create(); 
+
+const scripts = require("./scripts");
+const styles = require("./styles");
+
+var devMode = false;
+
+gulp.task("css", function() {
+    gulp.src(styles)
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+});
+
+gulp.task("js", function() {
+    gulp.src(scripts)
+    .pipe(concat("scripts.js"))
+    .pipe(gulp.dest("./dist/js"))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+});
+
+gulp.task("html", function() {
+    gulp.src("./src/**/*.html")
+        .pipe(gulp.dest("./dist/"))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task("build", function() {
+    gulp.series(["css","js","html"])
+})
+
+gulp.task("browser-sync", function() {
+    browserSync.init(null, {
+        open: false,
+        server: {
+            baseDir: "dist"
+        }
+    });
+});
+
+gulp.task("dev", function() {
+    devMode = true;
+    gulp.series(["build", "browser-sync"]);
+    gulp.watch(["./src/**/*.css"], ["css"]);
+    gulp.watch(["./src/**/*.js"], ["js"]);
+    gulp.watch(["./src/**/*.html"], ["htlm"]);
+})
