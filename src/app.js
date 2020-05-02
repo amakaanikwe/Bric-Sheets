00c74ca5438787af2c4fe18b9afc47b0604861c9
Amaka Anikwe <amakaanikwe@gmail.com>
2020-05-01 23:42:11 -0400

    Updated src app.js

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@ const app = angular.module('bricApp',['ngRoute','firebase','ui.bootstrap']);
 app.config (['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/login', {
-      templateUrl: '../src/login_registration/views/login.html',
+      templateUrl: './login_registration/views/login.html',
       controller: 'RegistrationController'
     })
     .when('/registration', {
-      templateUrl: '../src/login_registration/views/registration.html',
+      templateUrl: './login_registration/views/registration.html',
       controller: 'RegistrationController'
     })
     .when('/success', {
-      templateUrl: './public/js/partials/producerSearchList.html',
+      templateUrl: './producer_display/views/producerSearchList.html',
       controller: 'ProducerDisplayController'
     })
     .otherwise({
