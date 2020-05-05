fd65828f1e467b4a21bcf97a46e2ae0bc361592c
Amaka Anikwe <amakaanikwe@gmail.com>
2020-05-05 19:21:52 -0400

    Updated scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -77,7 +77,7 @@ app.controller("ProducerDisplayController", [
   "$http",
   "$uibModal",
   function ProducerDisplayController($scope,$http, $uibModal) {
-    $http.get('./data/data.json').then(function(response) { 
+    $http.get('./data/data/data.json').then(function(response) { 
       $scope.producers = response.data; 
       $scope.producerItems = response.data;
       $scope.filteredProducers = [],
